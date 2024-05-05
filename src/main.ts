8d4b0ca7b235bb46dac47b6cf1a290ea460be646
Karsten Zhou <54686209+XiaoSong-CPE@users.noreply.github.com>
2024-05-05 17:16:29 +0800

    Revert "add alert"
    This reverts commit a46d420b90345e3e7712b7a9ed0d12b0cb6d1f54.

--- src/main.ts
+++ src/main.ts
@@ -24,11 +24,3 @@ for (let index = 0; index < devList.length; index++) {
 }
 // inject dev to window
 unsafeWindow.dev = dev;
-// if http://127.0.0.1:5173 is not online, alert user to close the dev mode
-if (dev) {
-try {
-  fetch("http://127.0.0.1:5173")
-} catch (error) {
-  alert("开发服务器似乎没有开启，请关闭开发者模式（脚本猴 => dev-toggle-for-gitHub-code => 开发者模式: false）");
-}
-}
